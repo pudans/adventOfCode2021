ecbc36f1248afde621864f8859d6a837c4d7206b
konstantin <kkarpyshev@ivi.ru>
2021-12-15 20:27:16 +0300

    Day14

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -3,13 +3,13 @@ package day01
 import java.io.File
 import java.lang.StringBuilder
 
-data class Polymerization(val template: String, val map: MutableMap<String, Char>)
+data class Polymerization(val template: CharArray, val map: MutableMap<String, Char>)
 
 class Day14 : Base<Polymerization, Long>(14) {
 
     override fun mapInputData(file: File): Polymerization {
         val lines = file.readLines()
-        val template = lines.first()
+        val template = lines.first().toCharArray()
         val map = mutableMapOf<String, Char>()
         for (i in 2 until lines.size) {
             val splited = lines[i].split("->").map { it.trim() }
@@ -19,29 +19,30 @@ class Day14 : Base<Polymerization, Long>(14) {
     }
 
     override fun part1(input: Polymerization): Long {
-        val map = input.map
-        var temp = input.template
-        val steps = 10
-        println(temp)
-        repeat(steps) { step ->
-            println("step $step, len=${temp.length}")
-            val tempResult = StringBuilder()
-            tempResult.append(temp.first())
-            for (i in 0 until temp.lastIndex) {
-                val f1 = temp[i]
-                val f2 = temp[i+1]
-                val f3 = map["$f1$f2"]!!
-                tempResult.append("$f3$f2")
-            }
-            temp = tempResult.toString()
-        }
-        val set = temp.toCharArray().toSet().associateWith { ch -> temp.count { it == ch } }
-        println(set)
-        val min = set.minOf { it.value }
-        val max = set.maxOf { it.value }
-        println(max)
-        println(min)
-        return (max - min).toLong()
+//        val map = input.map
+//        var temp = input.template
+//        val steps = 10
+//        println(temp)
+//        repeat(steps) { step ->
+//            println("step $step, len=${temp.size}")
+//            val tempResult = StringBuilder()
+//            tempResult.append(temp.first())
+//            for (i in 0 until temp.lastIndex) {
+//                val f1 = temp[i]
+//                val f2 = temp[i+1]
+//                val f3 = map["$f1$f2"]!!
+//                tempResult.append("$f3$f2")
+//            }
+//            temp = tempResult.toString()
+//        }
+//        val set = temp.toCharArray().toSet().associateWith { ch -> temp.count { it == ch } }
+//        println(set)
+//        val min = set.minOf { it.value }
+//        val max = set.maxOf { it.value }
+//        println(max)
+//        println(min)
+//        return (max - min).toLong()
+        return 0L
     }
 
     /**
@@ -50,53 +51,55 @@ class Day14 : Base<Polymerization, Long>(14) {
      */
     override fun part2(input: Polymerization): Long {
         val map = input.map
-        var temps = listOf<String>(input.template)
+        var temps = listOf<CharArray>(input.template)
         val steps = 30
-
-        println(temps)
 
         repeat(steps) { step ->
             println("step $step, temps ${temps.size}")
-
-            val newTemps = mutableListOf<String>()
+            val tempStringResult = ArrayList<Char>(5_100)
+            val newTemps = mutableListOf<CharArray>()
 
             for (j in temps.indices) {
                 val temp = temps[j]
-                val tempStringResult = StringBuilder(2_100_000)
                 if (j == 0) {
-                    tempStringResult.append(temp.first())
+                    tempStringResult.add(temp.first())
                 } else {
                     val f1 = temps[j-1].last()
                     val f2 = temp.first()
                     val f3 = map["$f1$f2"]!!
-                    tempStringResult.append("$f3$f2")
+                    tempStringResult.add(f3)
+                    tempStringResult.add(f2)
                 }
 
                 for (i in 0 until temp.lastIndex) {
                     val f1 = temp[i]
                     val f2 = temp[i+1]
                     val f3 = map["$f1$f2"]!!
-                    tempStringResult.append("$f3$f2")
-                    if (tempStringResult.length > 2_000_000) {
-                        newTemps.add(tempStringResult.toString())
+                    tempStringResult.add(f3)
+                    tempStringResult.add(f2)
+                    if (tempStringResult.size > 5_000) {
+                        newTemps.add(tempStringResult.toCharArray())
                         tempStringResult.clear()
                     }
                 }
-                if (tempStringResult.isNotEmpty()) {
-                    newTemps.add(tempStringResult.toString())
+                if (j % 10 == 0) {
+                    System.gc()
                 }
-                System.gc()
             }
 
-            temps = newTemps
+            if (tempStringResult.isNotEmpty()) {
+                newTemps.add(tempStringResult.toCharArray())
+            }
+            System.gc()
 
+            temps = newTemps
         }
 
         println("CALCULATION")
 
         val set = HashMap<Char, Long>()
         temps.forEach { temp ->
-            temp.toCharArray().toSet().forEach { ch ->
+            temp.toSet().forEach { ch ->
                 set[ch] = set.getOrDefault(ch, 0L) + temp.count { it == ch }
             }
         }
